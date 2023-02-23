824ba9cee69cd8fde7a28f95eeb4554b7f201333
ZivWu1229 <zivwu2852@gmail.com>
2023-02-24 00:25:39 +0800

    v0.1.1

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.options import Options
 from json import loads,dumps
 import time
-from openpyxl import Workbook
+from openpyxl import Workbook,load_workbook
 import os
-import getpass
-import threading
+from selenium.webdriver.common.keys import Keys
 
 url = "https://meet.google.com/dke-mjah-gnw"
+config = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config')
 
 class Functions():
     def __init__(self) -> None:
-        while True:
-            answer = input('請選擇功能:\n(1 離開')
+            answer = input('請選擇功能:\n(1 離開\n')
             if answer == '1':
-                pass
+                self.quit()
     def quit(self):
         roll_call_list.save_file()
         driver.quit()
@@ -40,68 +39,83 @@ class Member_events():
             else:
                 self.member_left(members_for_now)
             self.members = members_for_now
-            return
+            roll_call_list.save_file()
     def member_joined(self,members_for_now)->None:
         for member in members_for_now:
             if not member in self.members:
-                roll_call_list.join(member)
                 print(member+'加入了!')
-                return
+                roll_call_list.join(member)
+                
     def member_left(self,members_for_now)->None:
         for member in self.members:
             if not member in members_for_now:
                 print(member+'離開了!')
                 roll_call_list.left(member)
-                return
+                
 
-class Roll_call_list(Workbook):
+class Roll_call_list():
     __file_name = ''
     def __init__(self):
-        self.__file_name = input("點名單檔案名稱:")+'.xlsx'
-        self.create()
+        self.__file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)),input("點名單檔案名稱:")+'.xlsx')
+
+        if os.path.isfile(self.__file_name):
+            self.load()
+        else:
+            self.new_workbook()
         self.save_file()
-    def create(self):
-        super().__init__()
-        worksheet = self.active
-        worksheet.merge_cells('A1:B1')
-        worksheet['A1']='加入'
-        worksheet.merge_cells('C1:D1')
-        worksheet['C1']='離開'
-        worksheet['E1']='點名紀錄'
-        worksheet.column_dimensions['A'].width = 25
-        worksheet.column_dimensions['B'].width = 20
-        worksheet.column_dimensions['C'].width = 25
-        worksheet.column_dimensions['D'].width = 20
+    def new_workbook(self):
+        self.workbook = Workbook()
+        self.worksheet = self.workbook.active
+        self.paint_sheet()
+    def load(self):
+        self.workbook = load_workbook(self.__file_name)
+        self.worksheet = self.workbook.create_sheet()
+        self.paint_sheet()
+    def paint_sheet(self):
+        local_time = time.localtime(time.time())
+        self.worksheet.title = f'{local_time.tm_hour}-{local_time.tm_min}'
+        self.worksheet.merge_cells('A1:B1')
+        self.worksheet['A1']='加入'
+        self.worksheet.merge_cells('C1:D1')
+        self.worksheet['C1']='離開'
+        self.worksheet['E1']='點名紀錄'
+        self.worksheet.column_dimensions['A'].width = 25
+        self.worksheet.column_dimensions['B'].width = 20
+        self.worksheet.column_dimensions['C'].width = 25
+        self.worksheet.column_dimensions['D'].width = 20
     def save_file(self):
         while True:
             try:
-                self.save(self.__file_name)
-            except:
+                self.workbook.save(self.__file_name)
+            except PermissionError:
                 input('無法儲存檔案，請關閉檔案後按enter重試')
+            except Exception as exception:
+                print('儲存檔案時發生錯誤:'+str(exception))
+                input('按Enter重試')
             else:
                 print('檔案儲存成功!')
                 return
     def join(self,user_name:str):
         current_cell = 2
-        worksheet = self.active
+        
         while True:
-            if worksheet['A'+str(current_cell)].value:
+            if self.worksheet['A'+str(current_cell)].value:
                 current_cell+=1
-                break
-            worksheet['A'+str(current_cell)] = time.ctime()
-            worksheet['B'+str(current_cell)] = user_name
-            self.save_file()
+                continue
+            
+            self.worksheet['A'+str(current_cell)] = time.ctime()
+            self.worksheet['B'+str(current_cell)] = user_name
+            
             return
     def left(self,user_name:str):
         current_cell = 2
-        worksheet = self.active
         while True:
-            if worksheet['C'+str(current_cell)].value:
+            if self.worksheet['C'+str(current_cell)].value:
                 current_cell+=1
                 continue
-            worksheet['C'+str(current_cell)] = time.ctime()
-            worksheet['D'+str(current_cell)] = user_name
-            self.save_file()
+            self.worksheet['C'+str(current_cell)] = time.ctime()
+            self.worksheet['D'+str(current_cell)] = user_name
+            
             return
 
 class Login_Google():
@@ -121,41 +135,36 @@ class Login_Google():
         driver.get(url)
     def login_with_cookie(self):
         driver.delete_all_cookies()
-        with open("config","r") as file:
+        with open(config,"r") as file:
             cookie = loads(file.read())
             for name,value in cookie.items():
                 driver.add_cookie({"name":name,"value":value})
     def manual_login(self):
+        driver.delete_all_cookies()
         driver.get("https://accounts.google.com/v3/signin/identifier?dsh=S-905868804%3A1676301694738053&_ga=2.207397329.35016544.1676301693-561019711.1676301693&continue=https%3A%2F%2Fmeet.google.com%3Fhs%3D193&ltmpl=meet&o_ref=https%3A%2F%2Fmeet.google.com%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AWnogHdWVd-qRecSxEULl_n6vHYB07-UsP6Iqy7uBGNwMSXZ3GGXmR3rUzCtiuwX1w4tF4PMLDRIDQ")
 
         driver.implicitly_wait(30)
 
         # Enter your username and password
+        
         username = driver.find_element(By.NAME,"identifier")
         username.send_keys(input('輸入您的gmail帳號:'))
+        username.send_keys(Keys.ENTER)
 
-        # Click the next button
-        next_button = driver.find_element(By.ID,"identifierNext")
-        next_button.click()
-
-        # Wait for the password field to load
-        # ...
-
+        # password
         driver.implicitly_wait(10)
 
         passwd = driver.find_element(By.NAME,"Passwd")
-        passwd.send_keys(getpass.getpass('輸入您的密碼:'))
+        passwd.send_keys(input('輸入您的密碼:'))
 
         # Click the next button
-        next_button = driver.find_element(By.ID,"passwordNext")
-        next_button.click()
-        driver.get(url)
-        driver.implicitly_wait(10)
+        passwd.send_keys(Keys.ENTER)
+        time.sleep(3)
         web_cookies = driver.get_cookies()
         cookies = {}
         for cookie in web_cookies:
             cookies[cookie['name']]=cookie['value']
-        with open('config','x')as file:
+        with open(config,'x')as file:
             file.write(dumps(cookies))
         self.login_with_cookie()
 
@@ -184,15 +193,13 @@ while True:
     else: 
         link = driver.find_element(By.CLASS_NAME,"QJgqC")
         link.click()
-        driver.implicitly_wait(4)
+        driver.implicitly_wait(30)
         driver.find_elements(By.CLASS_NAME,"boDUxc")[1].click()
-        driver.implicitly_wait(4)
         break
 print('會議加入成功!')
 time.sleep(1)
 os.system('cls')
 
-threading.Thread(Member_events().__init__()).start()
-threading.Thread(Functions().__init__()).start()
-    
+
+Member_events()
 #driver.close()
---
+++ readme.md
@@ -0,0 +1,39 @@
+<style>
+  .version{
+    margin-left: 1em;
+  }
+</style>
+<h1 align="center">Meet Roll Caller</h1>
+
+<p align="center">
+  <!--<img src="https://example.com/meet-roll-caller-logo.png" alt="Meet Roll Caller Logo" width="200">-->
+</p>
+
+<!--<p align="center">
+  Meet Roll Caller is a web application that automates attendance taking in Google Meet meetings. It uses the Google Meet API to retrieve the list of participants and allows the meeting organizer to take attendance with a single click. This saves time and eliminates the need for manual attendance tracking.
+</p>-->
+
+## 一、下載
+1. 下載後解壓縮
+2. 執行 `install modules.bat` 以安裝模組
+3. 執行 `start.bat` 以開啟軟體
+
+## 二、使用方式
+
+1. 輸入點名單的檔案名稱(不須包含`.xlsx`)
+2. 若為首次使用或登入憑證失效需要您輸入帳號與密碼登入(密碼未顯示為正常現象)
+3. 登入成功後即可使用
+
+## 三、注意事項
+
+1. 關閉軟體請直接關閉命令提示字元視窗
+2. 加入會議後請勿將請求權限及會議成員之彈窗關閉以免發生錯誤
+3. 若點名表無法儲存請檢查是否正在開啟該excel檔案。若無，請將錯誤訊息回報給開發者:<a href="mailto:zivwu2852@gmail.com">Email</a>
+
+## 四、更新紀錄
+<div class="version">
+  <h3>v0.1.1</h3>
+  <ol>
+    <li></li>
+  </ol>
+</div>
